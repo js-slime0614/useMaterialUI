00ea7df4fc2c9671dac08628e957f4d5d1223d38
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2022-10-19 15:23:08 +0900

    Feeds

--- mui/src/components/Feed.jsx
+++ mui/src/components/Feed.jsx
@@ -1,9 +1,48 @@
-import { Box } from '@mui/material'
+import { Favorite, MoreVert, Share } from '@mui/icons-material'
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material'
 import React from 'react'
 
 const Feed = () => {
     return (
-        <Box bgcolor="pink" flex={4} p={2}>Feed</Box>
+        <Box bgcolor="pink" flex={4} p={2}>
+            <Card>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{bgcolor: 'red' }} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label='settings'>
+                            <MoreVert/>
+                        </IconButton>
+                    }
+                    title=''
+                    subheader=''
+                    />
+                    <CardMedia
+                        component='img'
+                        height='20%'
+                        image='images/card1.jpeg'
+                        alt='Paella dish'
+                    />
+                    <CardContent>
+                        <Typography variant='body2' color='text.secondary'>
+                            This imporessive paella is a perfect party dish and a fun meal to cook
+                            together with your guests. Add 1 cup of frozen peas along with the mussels,
+                            if you like.
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label='add to favorites'>
+                            <Favorite/>
+                        </IconButton>
+                        <IconButton aria-label='share'>
+                            <Share/>
+                        </IconButton>
+                    </CardActions>
+            </Card>
+        </Box>
     )
 }
 
